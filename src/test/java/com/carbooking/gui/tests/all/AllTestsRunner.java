5da6c12a67a0d3e6b67a68238ea0733a92ceee9b
Oleh Hanziienko <ohanziienko@hotmail.com>
2026-03-24 09:15:24 +0100

    Add AllTestsRunner class for centralized execution of all test suites Добавлен AllTestsRunner для запуска всех тестов из одной точки

---
+++ src/test/java/com/carbooking/gui/tests/all/AllTestsRunner.java
@@ -0,0 +1,27 @@
+package com.carbooking.gui.tests.all;
+
+import com.carbooking.gui.tests.positive.LoginPositiveTests;
+import com.carbooking.gui.tests.positive.BookingPositiveTests;
+import com.carbooking.gui.tests.positive.RegistrationPositiveTests;
+import com.carbooking.gui.tests.negative.LoginNegativeTests;
+import com.carbooking.gui.tests.negative.BookingNegativeTests;
+import org.testng.annotations.Test;
+import org.testng.TestNG;
+
+public class AllTestsRunner {
+
+    @Test
+    public void runEverything() {
+        TestNG testng = new TestNG();
+        // Добавляем сюда все твои классы тестов
+        testng.setTestClasses(new Class[] {
+                LoginPositiveTests.class,
+                BookingPositiveTests.class,
+                RegistrationPositiveTests.class,
+                LoginNegativeTests.class,
+                BookingNegativeTests.class,
+                FullWorkflowTest.class
+        });
+        testng.run();
+    }
+}
